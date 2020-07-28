81986f0f2eef402a88531a4892519ac8bd8403fa
StanislavKaisin <8050361@gmail.com>
2020-07-28 21:21:32 +0300

    Updates

---
+++ precache-manifest.a7373b73a02f1dd86b47fd3adb41d319.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69ece40ee9fae029656ddec6165c26da",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "a5976fcf7986369e0b33",
+    "url": "/test/static/css/main.3c12648e.chunk.css"
+  },
+  {
+    "revision": "26583e7396715ca46b2a",
+    "url": "/test/static/js/2.dddc6cfa.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/test/static/js/2.dddc6cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5976fcf7986369e0b33",
+    "url": "/test/static/js/main.2e3edf25.chunk.js"
+  },
+  {
+    "revision": "29d9a3cf2ee130fa9d36",
+    "url": "/test/static/js/runtime-main.2c1f07ac.js"
+  }
+]);
